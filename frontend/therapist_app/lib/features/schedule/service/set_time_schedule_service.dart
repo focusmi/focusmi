d4389890a2fa0edfd413688153b29671c7d072e8
TDMWalpala <tharindudarshana2019@gmail.com>
2023-07-29 20:38:33 +0530

    Added fetchSchedules and createSchedule functions

--- frontend/therapist_app/lib/features/schedule/service/set_time_schedule_service.dart
+++ frontend/therapist_app/lib/features/schedule/service/set_time_schedule_service.dart
@@ -25,12 +25,35 @@ class TimeRange {
 }
 
 class SetTimeScheduleService {
-  static Future<List<Schedule>> fetchSchedules() async {
-    final response =
-        await http.get(Uri.parse('https://your-api-url.com/schedules'));
+  static Future<List<Schedule>> fetchSchedules(BuildContext context) async {
+    final userProvider = Provider.of<UserProvider>(context, listen: false);
+    final user = userProvider.user;
+    final response = await http.get(
+      Uri.parse('$uri/apis/schedule/time/${user.id}'),
+      headers: <String, String>{
+        'Content-Type': 'application/json; charset=UTF-8',
+        'x-auth-token': user.token,
+      },
+    );
+
     if (response.statusCode == 200) {
-      final List<dynamic> jsonData = jsonDecode(response.body);
-      return jsonData.map((data) => Schedule.fromJson(data)).toList();
+      final dynamic jsonData = jsonDecode(response.body);
+      // print(jsonData);
+
+      if (jsonData == null) {
+        // Return an empty list if the response body is null
+        return [];
+      }
+
+      if (jsonData is List) {
+        // Handle the case where the response is a list of schedules
+        return jsonData.map((data) => Schedule.newFromJson(data)).toList();
+      } else if (jsonData is Map<String, dynamic>) {
+        // Handle the case where the response is a single schedule object
+        return [Schedule.fromJson(jsonData)];
+      } else {
+        throw Exception('Invalid response format');
+      }
     } else {
       throw Exception('Failed to fetch schedules');
     }
@@ -38,7 +61,6 @@ class SetTimeScheduleService {
 
   static Future<void> createSchedule(
       Schedule schedule, BuildContext context) async {
-    print(schedule.toJson());
     final userProvider = Provider.of<UserProvider>(context, listen: false);
     final user = userProvider.user;
 
@@ -75,6 +97,13 @@ class Schedule {
     return Schedule(
       start: DateTime.parse(json['start']),
       end: DateTime.parse(json['end']),
+    );
+  }
+  
+  factory Schedule.newFromJson(Map<String, dynamic> json) {
+    return Schedule(
+      start: DateTime.parse(json['session_time']),
+      end: DateTime.parse(json['session_end_time']),
     );
   }
 }
