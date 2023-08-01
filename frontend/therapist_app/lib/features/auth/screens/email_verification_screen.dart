2e17098e3932a9434285f25bf70a84822fe312b4
TDMWalpala <tharindudarshana2019@gmail.com>
2023-08-01 23:16:56 +0530

    Refactor email verification screen for better user experience

--- frontend/therapist_app/lib/features/auth/screens/email_verification_screen.dart
+++ frontend/therapist_app/lib/features/auth/screens/email_verification_screen.dart
@@ -1,21 +1,75 @@
 import 'package:flutter/material.dart';
-import '../widgets/verification_form.dart';
+import 'package:therapist_app/constants/util.dart';
+import 'package:therapist_app/features/auth/screens/mobile_number_screen.dart';
+import 'package:therapist_app/features/auth/services/auth_service.dart';
+import 'package:therapist_app/features/auth/widgets/verification_form.dart';
 
-class EmailVerificationScreen extends StatelessWidget {
+
+
+class EmailVerificationScreen extends StatefulWidget {
+  final int verificationCode;
+  final String email;
+  final String password;
+  final String name;
+
+  EmailVerificationScreen({
+    required this.verificationCode,
+    required this.email,
+    required this.password,
+    required this.name,
+    required BuildContext context,
+  });
+
+  @override
+  _EmailVerificationScreenState createState() =>
+      _EmailVerificationScreenState();
+}
+
+class _EmailVerificationScreenState extends State<EmailVerificationScreen> {
+  final TextEditingController _otpController = TextEditingController();
+  final AuthService authService = AuthService();
+
   @override
   Widget build(BuildContext context) {
-    return Scaffold(
-      appBar: AppBar(
-        title: Text(''),
-      ),
-      body: VerificationForm(
-        verificationType: VerificationType.Email,
-        onSubmit: (code) {
-          // Add your email verification logic here
-          // For this example, let's just print the code
-          print('Email Verification Code: $code');
-        },
-      ),
-    );
+    {
+      return Scaffold(
+        appBar: AppBar(
+          title: Text(''),
+        ),
+        body: VerificationForm(
+          verificationType: VerificationType.Email,
+          onSubmit: (_otpController) {
+
+            _verifyOTP(_otpController);
+          },
+          // 
+        ),
+      );
+    }
+  }
+
+  void _verifyOTP(String code) {
+    int userEnteredOTP = int.tryParse(code) ?? 0;
+    if (userEnteredOTP == widget.verificationCode) {
+      // The user-entered OTP is correct, you can return true or perform further actions
+      print(widget.name);
+      authService.signUpUser(
+        context: context,
+        email: widget.email, 
+        password: widget
+            .password, 
+        name: widget.name, 
+      );
+      Navigator.pop(context, true);
+      Navigator.push(
+        context,
+        MaterialPageRoute(
+          builder: (context) => PhoneNumberInputPage(),
+        ),
+      );
+    } else {
+      // The user-entered OTP is incorrect, you can show an error message or handle it as needed
+      showSnackBar(context, 'Incorrect OTP');
+    }
   }
 }
