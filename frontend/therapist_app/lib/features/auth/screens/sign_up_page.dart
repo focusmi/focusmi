589327a38ae780b8c4f37ddf377e72abdcfb5c1a
TDMWalpala <tharindudarshana2019@gmail.com>
2023-08-01 23:20:15 +0530

    Added input validations, Generate random 4-digit code and send email functionalities

--- frontend/therapist_app/lib/features/auth/screens/sign_up_page.dart
+++ frontend/therapist_app/lib/features/auth/screens/sign_up_page.dart
@@ -1,8 +1,12 @@
+import 'dart:math';
+
 import 'package:flutter/material.dart';
 import 'package:therapist_app/features/auth/screens/email_verification_screen.dart';
 import 'package:therapist_app/features/auth/services/auth_service.dart';
 import 'package:therapist_app/common/widgets/custom_textfield.dart';
 import 'package:therapist_app/common/widgets/custom_button.dart';
+import 'package:therapist_app/features/auth/services/email_sender.dart';
+import 'package:therapist_app/validation/form_validators.dart';
 
 import '../../../constants/global_variables.dart';
 
@@ -28,13 +32,38 @@ class _SignUpPageState extends State<SignUpPage> {
     _nameController.dispose();
   }
 
-  void signUpUser() {
-    authService.signUpUser(
-      context: context,
-      email: _emailController.text,
-      password: _passwordController.text,
-      name: _nameController.text,
-    );
+  void signUpUser() async {
+    if (_signUpFormKey.currentState!.validate()) {
+      // Perform user registration using authService
+      // Send an email to the user
+      try {
+        // Generate a random 4-digit verification code
+        final random = Random();
+        final verificationCode = random.nextInt(9000) + 1000;
+
+        // Send the email with the verification code
+        await EmailSender.sendEmail(
+          recipientEmail: _emailController.text.trim(),
+          subject: 'Email Verification Code',
+          body: 'Your verification code is: $verificationCode',
+        );
+        // Now you can use the verificationCode for further verification, such as comparing it with the user's input.
+        Navigator.of(context).push(
+          MaterialPageRoute(
+            builder: (context) => EmailVerificationScreen(
+              verificationCode: verificationCode,
+              context: context,
+              email: _emailController.text,
+              password: _passwordController.text,
+              name: _nameController.text,
+            ),
+          ),
+        );
+      } catch (e) {
+        // Handle any errors that occurred during sending the email
+        print('Error sending email: $e');
+      }
+    }
   }
 
   @override
@@ -45,20 +74,26 @@ class _SignUpPageState extends State<SignUpPage> {
     return Scaffold(
       body: SingleChildScrollView(
         child: Container(
-          padding: EdgeInsets.all(screenSize.width * 0.05), // Adjust padding based on screen width
+          padding: EdgeInsets.all(
+              screenSize.width * 0.05), // Adjust padding based on screen width
           child: Column(
             crossAxisAlignment: CrossAxisAlignment.start,
             children: [
-              SizedBox(height: screenSize.height * 0.04), // Adjust spacing based on screen height
+              SizedBox(
+                  height: screenSize.height *
+                      0.04), // Adjust spacing based on screen height
               Text(
                 'Create your account',
                 style: TextStyle(
                   color: GlobalVariables.greyTextColor,
-                  fontSize: screenSize.width * 0.05, // Adjust font size based on screen width
+                  fontSize: screenSize.width *
+                      0.05, // Adjust font size based on screen width
                   fontWeight: FontWeight.w500,
                 ),
               ),
-              SizedBox(height: screenSize.height * 0.03), // Adjust spacing based on screen height
+              SizedBox(
+                  height: screenSize.height *
+                      0.03), // Adjust spacing based on screen height
               Form(
                 key: _signUpFormKey,
                 child: Column(
@@ -67,28 +102,37 @@ class _SignUpPageState extends State<SignUpPage> {
                     CustomTextField(
                       controller: _emailController,
                       hintText: 'Email',
+                      validator: FormValidators.validateEmail,
+                      trimEnabled: true,
                     ),
-                    SizedBox(height: screenSize.height * 0.02), // Adjust spacing based on screen height
+                    SizedBox(
+                        height: screenSize.height *
+                            0.02), // Adjust spacing based on screen height
                     CustomTextField(
                       controller: _nameController,
                       hintText: 'Full Name',
+                      validator: FormValidators.validateFullName,
+                      trimEnabled: false,
                     ),
-                    SizedBox(height: screenSize.height * 0.02), // Adjust spacing based on screen height
+                    SizedBox(
+                        height: screenSize.height *
+                            0.02), // Adjust spacing based on screen height
                     CustomTextField(
                       controller: _passwordController,
                       hintText: 'Password',
                       obscureText: true,
+                      validator: FormValidators.validatePassword,
+                      trimEnabled: true,
                     ),
-                    SizedBox(height: screenSize.height * 0.02), // Adjust spacing based on screen height
+                    SizedBox(
+                        height: screenSize.height *
+                            0.02), // Adjust spacing based on screen height
                     CustomButton(
                       text: 'Proceed',
                       onTap: () {
-                        // if (_signUpFormKey.currentState!.validate()) {
-                        //   signUpUser();
-                        // }
-                        Navigator.of(context).push(
-                          MaterialPageRoute(builder: (context) => EmailVerificationScreen()),
-                        );
+                        if (_signUpFormKey.currentState!.validate()) {
+                          signUpUser();
+                        }
                       },
                     ),
                   ],
