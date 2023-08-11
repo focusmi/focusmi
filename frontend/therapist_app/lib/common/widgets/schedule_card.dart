381a270b575668dd931c21ee07a9f29ac9abf628
TDMWalpala <tharindudarshana2019@gmail.com>
2023-08-11 11:34:03 +0530

    Added Page Transitions

--- frontend/therapist_app/lib/common/widgets/schedule_card.dart
+++ frontend/therapist_app/lib/common/widgets/schedule_card.dart
@@ -1,6 +1,7 @@
 import 'package:flutter/material.dart';
 // import 'package:google_fonts/google_fonts.dart';
 import 'package:intl/intl.dart';
+import 'package:page_transition/page_transition.dart';
 import 'package:provider/provider.dart';
 import 'package:therapist_app/constants/util.dart';
 import 'package:therapist_app/features/conference/conference.dart';
@@ -146,8 +147,7 @@ class ScheduleCard extends StatelessWidget {
       required String userId}) {
     Navigator.push(
       context,
-      MaterialPageRoute(
-          builder: (context) => VideoConferencePage(
+      PageTransition(type:PageTransitionType.topToBottom, child:  VideoConferencePage(
                 conferenceID: conferenceId,
                 userName: userName,
                 userId: userId,
