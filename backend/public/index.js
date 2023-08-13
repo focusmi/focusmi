d1dca52f58dae4f10d4afcbd8a71472d64ef6aa9
TDMWalpala <tharindudarshana2019@gmail.com>
2023-08-13 13:24:52 +0530

    updated

--- backend/public/index.js
+++ backend/public/index.js
@@ -27,8 +27,7 @@ app.use(cors({
 }))
 
 app.use(cors())
-// app.use(express.json())
-app.use(express.json({ limit: '10mb', extended: true, strict: false }));
+app.use(express.json())
 app.use(express.urlencoded({extended: true}))//url encoded bodies)
 app.use(bodyParser.json())
 app.use(express.static('public'))
