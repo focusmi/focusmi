014b8d7225fb48629140b99ca7f26044c38f638d
TDMWalpala <tharindudarshana2019@gmail.com>
2023-09-02 20:55:56 +0530

    Added widgetsBindingObserver for get user state

--- frontend/therapist_app/lib/main.dart
+++ frontend/therapist_app/lib/main.dart
@@ -1,6 +1,7 @@
 import 'package:therapist_app/constants/global_variables.dart';
 import 'package:therapist_app/features/auth/screens/auth_screen.dart';
 import 'package:therapist_app/features/auth/services/auth_service.dart';
+import 'package:therapist_app/features/home/service/homeService.dart';
 import 'package:therapist_app/provider/user_provider.dart';
 import 'package:therapist_app/router.dart';
 import 'package:flutter/material.dart';
@@ -9,7 +10,7 @@ import 'common/widgets/bottom_bar.dart';
 import 'package:flutter_native_splash/flutter_native_splash.dart';
 import 'package:flutter_dotenv/flutter_dotenv.dart';
 
-void main() async{
+void main() async {
   WidgetsBinding widgetsBinding = WidgetsFlutterBinding.ensureInitialized();
   FlutterNativeSplash.preserve(widgetsBinding: widgetsBinding);
   await dotenv.load(fileName: ".env");
@@ -26,7 +27,6 @@ void main() async{
   FlutterNativeSplash.remove();
 }
 
-
 class MyApp extends StatefulWidget {
   const MyApp({Key? key}) : super(key: key);
 
@@ -34,7 +34,7 @@ class MyApp extends StatefulWidget {
   State<MyApp> createState() => _MyAppState();
 }
 
-class _MyAppState extends State<MyApp> {
+class _MyAppState extends State<MyApp> with WidgetsBindingObserver {
   final AuthService authService = AuthService();
 
   @override
@@ -43,6 +43,23 @@ class _MyAppState extends State<MyApp> {
     WidgetsBinding.instance.addPostFrameCallback((_) {
       authService.getUserData(context);
     });
+    WidgetsBinding.instance.addObserver(this);
+    setStatus(true);
+  }
+
+  void setStatus(bool status) async {
+    status
+        ? HomeService.updateUserState(state: 'online', context: context)
+        : HomeService.updateUserState(state: 'offline', context: context);
+  }
+
+  @override
+  void didChangeAppLifecycleState(AppLifecycleState state) {
+    if (state == AppLifecycleState.resumed) {
+      setStatus(true);
+    } else {
+      setStatus(false);
+    }
   }
 
   @override
