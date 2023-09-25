59c438667491c89d07042364cd20b69e50e3c7ed
TDMWalpala <tharindudarshana2019@gmail.com>
2023-09-25 20:04:20 +0530

    Added chart and updated home screen

--- frontend/therapist_app/lib/features/home/screen/home_screen.dart
+++ frontend/therapist_app/lib/features/home/screen/home_screen.dart
@@ -1,3 +1,4 @@
+import 'package:fl_chart/fl_chart.dart';
 import 'package:therapist_app/provider/user_provider.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter/widgets.dart';
@@ -16,7 +17,7 @@ class HomeScreen extends StatefulWidget {
 class _HomeScreenState extends State<HomeScreen> {
   @override
   Widget build(BuildContext context) {
-    return const SingleChildScrollView(
+    return SingleChildScrollView(
       child: Padding(
         padding: EdgeInsets.only(top: 40),
         child: Column(
@@ -25,6 +26,168 @@ class _HomeScreenState extends State<HomeScreen> {
             Padding(
               padding: EdgeInsets.symmetric(horizontal: 15),
               child: UserInfo(),
+            ),
+            Padding(
+              padding: const EdgeInsets.all(10.0),
+              child: Row(
+                children: [
+                  GestureDetector(
+                    onTap: () {
+                      print("Male");
+                      setState(() {
+                        // gender = "Male";
+                      });
+                    },
+                    child: Container(
+                      padding: const EdgeInsets.all(8.0),
+                      // color: Colors.orange.withAlpha(50),
+                      height: 100,
+                      width: 175,
+                      decoration: BoxDecoration(
+                          color:
+                              Color.fromARGB(255, 102, 255, 0).withAlpha(150),
+                          borderRadius: BorderRadius.circular(20),
+                          boxShadow: [
+                            BoxShadow(
+                              color: Color.fromARGB(255, 236, 236, 236),
+                              offset: Offset(0.0, 2.0), // Offset of the shadow
+                              blurRadius: 4.0, // Blur radius
+                              spreadRadius: 1.0, // Spread radius
+                            ),
+                          ]),
+                      child: Column(
+                        children: [
+                          Padding(
+                            padding: const EdgeInsets.only(
+                                left: 8.0, right: 8, top: 8, bottom: 1),
+                            child: Row(
+                              children: [
+                                Text(
+                                  "Appointments",
+                                  style: TextStyle(
+                                      fontSize: 15,
+                                      fontWeight: FontWeight.w400),
+                                ),
+                                const SizedBox(
+                                  width: 5,
+                                ),
+                                Icon(
+                                  Icons.info_rounded,
+                                  size: 15,
+                                )
+                              ],
+                            ),
+                          ),
+                          Column(
+                            children: [
+                              Text(
+                                "100",
+                                style: TextStyle(
+                                  fontSize: 30,
+                                  fontWeight: FontWeight.bold,
+                                ),
+                                textAlign: TextAlign.center,
+                              )
+                            ],
+                          ),
+                        ],
+                      ),
+                    ),
+                  ),
+                  const Spacer(),
+                  GestureDetector(
+                    onTap: () {
+                      print("Female");
+                      setState(() {});
+                    },
+                    child: Container(
+                      padding: const EdgeInsets.all(8.0),
+                      decoration: BoxDecoration(
+                          color:
+                              Color.fromARGB(255, 102, 255, 0).withAlpha(150),
+                          borderRadius: BorderRadius.circular(20),
+                          boxShadow: [
+                            BoxShadow(
+                              color: Color.fromARGB(255, 236, 236, 236),
+                              offset: Offset(0.0, 2.0), // Offset of the shadow
+                              blurRadius: 4.0, // Blur radius
+                              spreadRadius: 1.0, // Spread radius
+                            ),
+                          ]),
+                      height: 100,
+                      width: 175,
+                      child: Column(
+                        children: [
+                          Padding(
+                            padding: const EdgeInsets.only(
+                                left: 8.0, right: 8, top: 8, bottom: 1),
+                            child: Row(
+                              children: [
+                                Text("Total Clients",
+                                    style: TextStyle(
+                                      fontSize: 15,
+                                      fontWeight: FontWeight.w400,
+                                    )),
+                                const SizedBox(
+                                  width: 5,
+                                ),
+                                Icon(
+                                  Icons.info_rounded,
+                                  size: 15,
+                                )
+                              ],
+                            ),
+                          ),
+                          Column(
+                            children: [
+                              Text(
+                                "56",
+                                style: TextStyle(
+                                  fontSize: 30,
+                                  fontWeight: FontWeight.bold,
+                                ),
+                                textAlign: TextAlign.center,
+                              )
+                            ],
+                          ),
+                        ],
+                      ),
+                    ),
+                  ),
+                ],
+              ),
+            ),
+            Padding(
+              padding: const EdgeInsets.all(8.0),
+              child: Container(
+                width: MediaQuery.of(context).size.width - 20,
+                height: 200,
+                decoration: BoxDecoration(
+                  // color: Color.fromARGB(255, 255, 216, 216),
+                ),
+                child: ListView(
+                  scrollDirection: Axis.horizontal,
+                  children: [
+                  // BarChartSample(),
+                  // BarChartSample(),
+                  // BarChartSample(),
+                  Container(
+                    width: MediaQuery.of(context).size.width - 50,
+                    child:  Padding(
+                      padding: const EdgeInsets.all(8.0),
+                      child: BarChartSample(),
+                    ),
+                  ),
+                  Container(
+                    width: MediaQuery.of(context).size.width - 50,
+                    child:  Padding(
+                      padding: const EdgeInsets.all(8.0),
+                      child: BarChartSample(),
+                    ),
+                  ),
+                  ]
+                  ),
+              ),
             ),
           ],
         ),
@@ -57,36 +220,142 @@ class _UserInfoState extends State<UserInfo> {
             .headlineSmall!
             .copyWith(fontWeight: FontWeight.w700),
       ),
-      trailing: Container(
-        width: 48.0,
-        height: 48.0,
-        decoration: const BoxDecoration(
-          // image: DecorationImage(
-          //   image: AssetImage("assets/images/doctor1.jpg"),
-          //   fit: BoxFit.cover,
-          //   repeat: ImageRepeat.repeat,
-          // ),
-          borderRadius: BorderRadius.all(Radius.circular(18.0)),
+    );
+  }
+}
+
+class BarChartSample extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      body: Container(
+        decoration: BoxDecoration(
+          borderRadius: BorderRadius.circular(10),
+          color: Color.fromARGB(255, 187, 250, 195)
         ),
-        child: Stack(
-          alignment: Alignment.topRight,
-          children: [
-            Container(
-              width: 18.0,
-              height: 18.0,
-              decoration: BoxDecoration(
-                shape: BoxShape.circle,
-                color: GlobalVariables.primarySwatch,
-                border: Border.all(
-                  color: Colors.white,
-                  width: 3.0,
-                  style: BorderStyle.solid,
+        child: Center(
+          child: Padding(
+            padding: const EdgeInsets.all(10.0),
+            child: BarChart(
+              BarChartData(
+                alignment: BarChartAlignment.spaceAround,
+                maxY: 12, // Adjust the maximum y-axis value as needed
+                gridData: FlGridData(
+                  show: false,
                 ),
+                backgroundColor: Color.fromARGB(0, 255, 255, 255),
+                barTouchData: BarTouchData(
+                  touchTooltipData: BarTouchTooltipData(
+                    tooltipBgColor: GlobalVariables.primaryText,
+                    tooltipRoundedRadius: 10,
+                  ),
+                ),
+                titlesData: FlTitlesData(
+                  leftTitles: SideTitles(showTitles: false),
+                  rightTitles: SideTitles(showTitles: false),
+                  topTitles: SideTitles(showTitles: false),
+                  bottomTitles: SideTitles(
+                    showTitles: true,
+                    margin: 10,
+                    getTitles: (double value) {
+                      switch (value.toInt()) {
+                        case 0:
+                          return 'Sun';
+                        case 1:
+                          return 'Mon';
+                        case 2:
+                          return 'Tue';
+                        case 3:
+                          return 'Wed';
+                        case 4:
+                          return 'Thu';
+                        case 5:
+                          return 'Fri';
+                        case 6:
+                          return 'Sat';
+                        default:
+                          return '';
+                      }
+                    },
+                  ),
+                ),
+                borderData: FlBorderData(show: false),
+                axisTitleData: FlAxisTitleData(show: false),
+      
+                barGroups: getBarGroups(),
               ),
             ),
-          ],
+          ),
         ),
       ),
     );
+  }
+
+  List<BarChartGroupData> getBarGroups() {
+    return [
+      BarChartGroupData(
+        x: 0,
+        barRods: [
+          BarChartRodData(
+            y: 4, // Value for Sunday
+            colors: Colors.primaries,
+          ),
+        ],
+      ),
+      BarChartGroupData(
+        x: 1,
+        barRods: [
+          BarChartRodData(
+            y: 6, // Value for Monday
+            // color: Colors.green,
+          ),
+        ],
+      ),
+      BarChartGroupData(
+        x: 2,
+        barRods: [
+          BarChartRodData(
+            y: 8, // Value for Tuesday
+            // color: Colors.orange,
+          ),
+        ],
+      ),
+      BarChartGroupData(
+        x: 3,
+        barRods: [
+          BarChartRodData(
+            y: 5, // Value for Wednesday
+            //color: Colors.red,
+          ),
+        ],
+      ),
+      BarChartGroupData(
+        x: 4,
+        barRods: [
+          BarChartRodData(
+            y: 9, // Value for Thursday
+            // color: Colors.purple,
+          ),
+        ],
+      ),
+      BarChartGroupData(
+        x: 5,
+        barRods: [
+          BarChartRodData(
+            y: 7, // Value for Friday
+            // color: Colors.teal,
+          ),
+        ],
+      ),
+      BarChartGroupData(
+        x: 6,
+        barRods: [
+          BarChartRodData(
+            y: 3, // Value for Saturday
+            // color: Colors.amber,
+          ),
+        ],
+      ),
+    ];
   }
 }
