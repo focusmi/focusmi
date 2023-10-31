cf5d0b1daacf16e2ebb626a51a1b81df2a825c66
TDMWalpala <tharindudarshana2019@gmail.com>
2023-10-31 14:04:29 +0530

    Added count schedule func

--- frontend/therapist_app/lib/features/home/service/homeService.dart
+++ frontend/therapist_app/lib/features/home/service/homeService.dart
@@ -35,4 +35,28 @@ class HomeService {
       return false;
     }
   }
+
+  static Future<String> getScheduleCount({
+    required BuildContext context,
+    }) async { 
+    try {
+      final userProvider = Provider.of<UserProvider>(context, listen: false);
+      final user = userProvider.user;
+      final response = await http.get(
+        Uri.parse('$uri/apis/schedule/count/${user.id}'),
+        headers: <String, String>{
+          'Content-Type': 'application/json; charset=UTF-8',
+          'x-auth-token': user.token,
+        },
+      );
+
+      if (response.statusCode == 200) {
+        return json.decode(response.body);
+      } else {
+        throw Exception('Failed to load schedule count data');
+      }
+    } catch (e) {
+      throw Exception('Error fetching schedule data: $e');
+    }
+  }
 }
