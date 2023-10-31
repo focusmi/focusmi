f7c6f5c0cec629d0febbc8ada6e2e03b214941fa
TDMWalpala <tharindudarshana2019@gmail.com>
2023-10-31 14:03:29 +0530

    Updated  home screen

--- frontend/therapist_app/lib/features/home/screen/home_screen.dart
+++ frontend/therapist_app/lib/features/home/screen/home_screen.dart
@@ -1,7 +1,12 @@
 import 'package:lottie/lottie.dart';
 import 'package:therapist_app/common/widgets/custom_button.dart';
+import 'package:therapist_app/features/blog/screen/blog_screen.dart';
+import 'package:therapist_app/features/blog/screen/create_blog_screen.dart';
 import 'package:therapist_app/features/home/screen/charts.dart';
+import 'package:therapist_app/features/home/service/homeService.dart';
+import 'package:therapist_app/features/profile/screen/profile_detail_screen.dart';
 import 'package:therapist_app/features/schedule/screen/schedule_screen.dart';
+import 'package:therapist_app/features/schedule/screen/set_time_schedule_screen.dart';
 import 'package:therapist_app/provider/user_provider.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter/widgets.dart';
@@ -16,8 +21,190 @@ class HomeScreen extends StatefulWidget {
 }
 
 class _HomeScreenState extends State<HomeScreen> {
+  late String newCount = '';
+
   @override
+  void initState() {
+    super.initState();
+    fetchCount();
+  }
+
+  fetchCount() async {
+    try {
+      String count = await HomeService.getScheduleCount(context: context);
+      setState(() {
+        newCount = count;
+      });
+    } catch (e) {
+      print('Error fetching data: $e');
+    }
+  }
+
+  @override
+
   Widget build(BuildContext context) {
+    List<Widget> buttonContainers = [
+      GestureDetector(
+        onTap: () async {
+             final result = await Navigator.push(
+                  context,
+                  MaterialPageRoute(
+                    builder: (context) => CreateBlog(),
+                  ),
+                );
+
+                 if (result == true) {
+                  Navigator.push(
+                  context,
+                  MaterialPageRoute(
+                    builder: (context) => BlogScreen(),
+                  ),
+                );
+                }
+        },
+        child: Container(
+          height: 100,
+          width: 120,
+          margin: EdgeInsets.all(10),
+          decoration: BoxDecoration(
+            color: Color.fromARGB(255, 255, 255, 255),
+            borderRadius: BorderRadius.circular(10),
+            boxShadow: [
+              BoxShadow(
+                color: Color.fromARGB(255, 236, 236, 236),
+                offset: Offset(0.0, 2.0),
+                blurRadius: 4.0,
+                spreadRadius: 1.0,
+              ),
+            ],
+          ),
+          child: Column(
+            children: [
+              Center(
+                child: Container(
+                  height: 80,
+                  child: Lottie.asset(
+                    'assets/images/3JYUHR2r2k.json',
+                    fit: BoxFit.cover,
+                  ),
+                ),
+              ),
+              Container(
+                child: Text(
+                  'Add Blog',
+                  textAlign: TextAlign.center,
+                  style: TextStyle(
+                    fontSize: 18,
+                    fontWeight: FontWeight.w400,
+                  ),
+                ),
+              ),
+            ],
+          ),
+        ),
+      ),
+      GestureDetector(
+        onTap: () {
+         Navigator.push(
+                  context,
+                  MaterialPageRoute(
+                    builder: (context) => ProfileDetailsPage(),
+                  ),
+                );
+        },
+        child: Container(
+          height: 100,
+          width: 120,
+          margin: EdgeInsets.all(10),
+          decoration: BoxDecoration(
+            color: Color.fromARGB(255, 255, 255, 255),
+            borderRadius: BorderRadius.circular(10),
+            boxShadow: [
+              BoxShadow(
+                color: Color.fromARGB(255, 236, 236, 236),
+                offset: Offset(0.0, 2.0),
+                blurRadius: 4.0,
+                spreadRadius: 1.0,
+              ),
+            ],
+          ),
+          child: Column(
+            children: [
+              Center(
+                child: Container(
+                  height: 80,
+                  child: Lottie.asset(
+                    'assets/images/Animation - 1698730732018.json',
+                    fit: BoxFit.cover,
+                  ),
+                ),
+              ),
+              Container(
+                child: Text(
+                  'My Account',
+                  textAlign: TextAlign.center,
+                  style: TextStyle(
+                    fontSize: 18,
+                    fontWeight: FontWeight.w400,
+                  ),
+                ),
+              ),
+            ],
+          ),
+        ),
+      ),
+      GestureDetector(
+        onTap: () {
+          Navigator.push(
+                  context,
+                  MaterialPageRoute(
+                    builder: (context) => SetTimeScheduleScreen(),
+                  ),
+                );
+        },
+        child: Container(
+          height: 100,
+          width: 120,
+          margin: EdgeInsets.all(10),
+          decoration: BoxDecoration(
+            color: Color.fromARGB(255, 255, 255, 255),
+            borderRadius: BorderRadius.circular(10),
+            boxShadow: [
+              BoxShadow(
+                color: Color.fromARGB(255, 236, 236, 236),
+                offset: Offset(0.0, 2.0),
+                blurRadius: 4.0,
+                spreadRadius: 1.0,
+              ),
+            ],
+          ),
+          child: Column(
+            children: [
+              Center(
+                child: Container(
+                  height: 80,
+                  child: Lottie.asset(
+                    'assets/images/Animation - 1698604941220.json',
+                    fit: BoxFit.cover,
+                  ),
+                ),
+              ),
+              Container(
+                child: Text(
+                  'Time Slots',
+                  textAlign: TextAlign.center,
+                  style: TextStyle(
+                    fontSize: 18,
+                    fontWeight: FontWeight.w400,
+                  ),
+                ),
+              ),
+            ],
+          ),
+        ),
+      ),
+    ];
+
     return SingleChildScrollView(
       child: Padding(
         padding: EdgeInsets.only(top: 40),
@@ -131,7 +318,7 @@ class _HomeScreenState extends State<HomeScreen> {
                         Column(
                           children: [
                             Text(
-                              "100",
+                              '${newCount != '' ? newCount : '0'}',
                               style: TextStyle(
                                 fontSize: 30,
                                 fontWeight: FontWeight.bold,
@@ -256,7 +443,7 @@ class _HomeScreenState extends State<HomeScreen> {
                 "Time Slot Analyst",
                 style: TextStyle(
                   fontSize: 20,
-                  fontWeight: FontWeight.normal,
+                  fontWeight: FontWeight.bold,
                 ),
               ),
             ),
@@ -270,6 +457,48 @@ class _HomeScreenState extends State<HomeScreen> {
                   child: Padding(
                     padding: const EdgeInsets.all(8.0),
                     child: BarChartSample(),
+                  ),
+                ),
+              ),
+            ),
+            Padding(
+              padding: const EdgeInsets.only(left: 15, right: 10, top: 10),
+              child: Container(
+                child: Text(
+                  'Quick Access',
+                  style: TextStyle(
+                    fontSize: 20,
+                    fontWeight: FontWeight.bold,
+                  ),
+                ),
+              ),
+            ),
+            Padding(
+              padding: const EdgeInsets.only(left: 15, right: 15, top: 10),
+              child: Container(
+                height: 160,
+                constraints: BoxConstraints(
+                  maxHeight: MediaQuery.of(context).size.height,
+                ),
+                decoration: BoxDecoration(
+                    color: Color.fromARGB(255, 243, 255, 243),
+                    borderRadius: BorderRadius.circular(10),
+                    boxShadow: [
+                      BoxShadow(
+                        color: Color.fromARGB(255, 236, 236, 236),
+                        offset: Offset(0.0, 2.0), // Offset of the shadow
+                        blurRadius: 4.0, // Blur radius
+                        spreadRadius: 1.0, // Spread radius
+                      ),
+                    ]),
+                child: Padding(
+                  padding: const EdgeInsets.all(5.0),
+                  child: ListView.builder(
+                    scrollDirection: Axis.horizontal,
+                    itemCount: buttonContainers.length,
+                    itemBuilder: (BuildContext context, int index) {
+                      return buttonContainers[index];
+                    },
                   ),
                 ),
               ),
@@ -289,6 +518,27 @@ class UserInfo extends StatefulWidget {
 }
 
 class _UserInfoState extends State<UserInfo> {
+  String greeth() {
+    var hour = DateTime.now().hour;
+
+    if (hour <= 12) {
+      return 'Good Morning';
+    } else if ((hour > 12) && (hour <= 16)) {
+      return 'Good Afternoon';
+    } else if ((hour > 16) && (hour < 20)) {
+      return 'Good Evening';
+    } else {
+      return 'Good Night';
+    }
+  }
+
+  var greets = '';
+  @override
+  void initState() {
+    greets = greeth();
+    super.initState();
+  }
+
   @override
   Widget build(BuildContext context) {
     final user = Provider.of<UserProvider>(context).user;
@@ -297,8 +547,8 @@ class _UserInfoState extends State<UserInfo> {
         Container(
           height: 120,
           width: 120,
-          child: Lottie.asset('assets/images/qO4KLdZrLb.json',
-              fit: BoxFit.cover),
+          child:
+              Lottie.asset('assets/images/qO4KLdZrLb.json', fit: BoxFit.cover),
         ),
         Padding(
           padding: const EdgeInsets.only(left: 20),
@@ -306,7 +556,11 @@ class _UserInfoState extends State<UserInfo> {
             mainAxisAlignment: MainAxisAlignment.start,
             crossAxisAlignment: CrossAxisAlignment.start,
             children: [
-              Text("Good Morning",textAlign: TextAlign.start, style: TextStyle(fontSize: 16),),
+              Text(
+                "${greets}",
+                textAlign: TextAlign.start,
+                style: TextStyle(fontSize: 16),
+              ),
               Text(
                 "${user.name}",
                 style: Theme.of(context)
